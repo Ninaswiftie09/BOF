f429693a85d7ae27eb5d824a67693178d8204c6d
Ninaswiftie09 <naj231088@uvg.edu.gt>
2025-04-08 16:25:49 -0600

    estilos, colores, tipografías

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  
+    
+    <HelloWorld msg="Bienvenido Abril Uniformes y Bordados" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+import './assets/styles.css'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
