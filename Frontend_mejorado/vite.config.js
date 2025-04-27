52fd7dba2fec9eb29a6335693500143c92fdad85
Ninaswiftie09 <naj231088@uvg.edu.gt>
2025-04-27 13:09:15 -0600

    Configuración para que se quede solo un docker

--- Frontend_mejorado/vite.config.js
+++ Frontend_mejorado/vite.config.js
@@ -1,10 +1,9 @@
+// vite.config.js
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,7 +11,16 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+      },
     },
   },
 })
