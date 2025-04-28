f10be72cec6713de6ce557e9dc115172c47cff76
Alejandrosiaus <riv23674@uvg.edu.gt>
2025-04-27 19:18:10 -0600

    registro de modelos en el panel de administracion

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categoria, Producto, Venta, DetalleVenta
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Producto)
+admin.site.register(Venta)
+admin.site.register(DetalleVenta)
