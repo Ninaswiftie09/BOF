ea314ecacd80e2cfa962c704309ed904a955259e
Alejoram2505 <Alejoram2505@gmail.com>
2025-04-27 23:10:45 -0600

    se agregan las tablas para pedidos

--- Backend/ddl.sql
+++ Backend/ddl.sql
@@ -24,3 +24,20 @@ CREATE TABLE clientes (
 CREATE INDEX idx_clientes_nombre ON clientes(nombre);
 CREATE INDEX idx_clientes_telefono ON clientes(telefono);
 CREATE INDEX idx_clientes_nit ON clientes(nit);
+
+-- Tabla de Pedidos (Ventas)
+CREATE TABLE pedidos (
+    id SERIAL PRIMARY KEY,
+    cliente_id INTEGER REFERENCES clientes(id) ON DELETE CASCADE,
+    fecha TIMESTAMP NOT NULL DEFAULT NOW(),
+    precio_total DECIMAL(12,2) NOT NULL
+);
+
+-- Detalle de Pedidos
+CREATE TABLE pedido_detalle (
+    id SERIAL PRIMARY KEY,
+    pedido_id INTEGER REFERENCES pedidos(id) ON DELETE CASCADE,
+    descripcion_producto TEXT,
+    cantidad INTEGER,
+    precio_unitario DECIMAL(12,2)
+);
