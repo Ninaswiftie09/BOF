d3243823f4de12be2a8b89f32c44c009a2ae9810
Alejoram2505 <Alejoram2505@gmail.com>
2025-05-21 22:29:33 -0600

    se conecto el view con los serializers de las tablas

--- Backend/clientes/views.py
+++ Backend/clientes/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Empresa, Cliente, Pedido, PedidoDetalle, CuentaPagada, Proveedor, Compra, CompraDetalle
+from .serializers import (
+    EmpresaSerializer, ClienteSerializer, PedidoSerializer, PedidoDetalleSerializer,
+    CuentaPagadaSerializer, ProveedorSerializer, CompraSerializer, CompraDetalleSerializer
+)
 
-# Create your views here.
+class EmpresaViewSet(viewsets.ModelViewSet):
+    queryset = Empresa.objects.all()
+    serializer_class = EmpresaSerializer
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
+
+class PedidoDetalleViewSet(viewsets.ModelViewSet):
+    queryset = PedidoDetalle.objects.all()
+    serializer_class = PedidoDetalleSerializer
+
+class CuentaPagadaViewSet(viewsets.ModelViewSet):
+    queryset = CuentaPagada.objects.all()
+    serializer_class = CuentaPagadaSerializer
+
+class ProveedorViewSet(viewsets.ModelViewSet):
+    queryset = Proveedor.objects.all()
+    serializer_class = ProveedorSerializer
+
+class CompraViewSet(viewsets.ModelViewSet):
+    queryset = Compra.objects.all()
+    serializer_class = CompraSerializer
+
+class CompraDetalleViewSet(viewsets.ModelViewSet):
+    queryset = CompraDetalle.objects.all()
+    serializer_class = CompraDetalleSerializer
