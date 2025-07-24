626c814b8ac4b441989b6e243a944a6f303ed3bb
Alejoram2505 <Alejoram2505@gmail.com>
2025-07-24 17:10:01 -0600

    cambio para la atorización de los metodos

--- Backend/proyecto_django/settings.py
+++ Backend/proyecto_django/settings.py
@@ -65,6 +65,16 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
