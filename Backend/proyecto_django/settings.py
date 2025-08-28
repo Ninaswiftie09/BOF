e1c5fb10458db83aec1886ef9ddcd094ede7863d
Alejoram2505 <Alejoram2505@gmail.com>
2025-08-27 19:12:50 -0600

    uso de credenciales del cliente

--- Backend/proyecto_django/settings.py
+++ Backend/proyecto_django/settings.py
@@ -165,8 +165,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dr378535@gmail.com'
-EMAIL_HOST_PASSWORD = 'yqye wefp kjvn tqki'
-DEFAULT_FROM_EMAIL = 'Abril Uniformes <dr378535@gmail.com>'
+EMAIL_HOST_USER = 'multiproductosdyr@gmail.com'
+EMAIL_HOST_PASSWORD = 'majp jfhs sutk cddo'
+DEFAULT_FROM_EMAIL = 'Abril Uniformes <multiproductosdyr@gmail.com>'
 
 
