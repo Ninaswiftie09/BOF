477f136ef248b44892fc46a3a04687956765fbed
Alejoram2505 <Alejoram2505@gmail.com>
2025-09-12 09:13:44 -0600

    cambios de refactorización de endpoints

--- Backend/proyecto_django/urls.py
+++ Backend/proyecto_django/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/cliente/', include('clientes.urls')), 
     path("api/", include("api.urls")), 
 ]
